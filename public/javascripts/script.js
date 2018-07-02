427118068abb054ab0c31fb68f19f5cc5afe5d03
Calin Segarceanu <section11@Calins-MacBook-Pro.local>
2018-07-01 21:58:51 -0500

    Display stock info for each stock

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $( "ul#stockList li" ).click(function() {
     const uri = '/getstock/' + $(this).attr("data-stock");
     $.get(uri, function(data) {
-      alert(data);
+      $("#stockcontent").html(data);
      });
   });
 });
